cc3cef91ca59b49b52e9669a838be9a3a5c3155a
mattietea <mattcthomas@me.com>
2018-07-25 08:53:59 -0700

    Add todo comment above Apollo init function to fix format error.

--- src/services/apollo.service.js
+++ src/services/apollo.service.js
@@ -3,6 +3,10 @@ import * as resolvers from '../modules/**/*.*resolvers.js';
 import * as typeDefs from '../modules/**/*.*typeDefs.gql';
 import { createSchema } from './utility.service.js';
 
+/**
+ * TODO: Fix format error once its fixed on the Apollo repo
+ * Issue: https://github.com/apollographql/graphql-tools/issues/890
+ */
 export const apolloInit = () => {
   const server = new ApolloServer({
     schema: createSchema(typeDefs, resolvers),
